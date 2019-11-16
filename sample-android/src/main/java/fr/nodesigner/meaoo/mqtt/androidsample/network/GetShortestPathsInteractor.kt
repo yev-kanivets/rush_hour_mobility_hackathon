1568f65bae479036ee8624b188e5b78ccadb4d82
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2019-11-16 20:01:53 +0100

    Display shortest paths.

--- sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/GetShortestPathsInteractor.kt
+++ sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/GetShortestPathsInteractor.kt
@@ -6,34 +6,24 @@ import fr.nodesigner.meaoo.mqtt.androidsample.entity.Transport
 
 class GetShortestPathsInteractor {
 
-    suspend fun execute(request: GraphService.Request): List<PathAdapter.Path> {
-        val list = listOf(
-            GraphClient.getShortestPathWalk(request).body(),
-            GraphClient.getShortestPathSubway(request).body(),
-            GraphClient.getShortestPathBike(request).body(),
-            GraphClient.getShortestPathCar(request).body()
-        )
+    suspend fun execute(request: GraphService.Request) = listOfNotNull(
+        GraphClient.getShortestPathWalk(request).body(),
+        GraphClient.getShortestPathSubway(request).body(),
+        GraphClient.getShortestPathBike(request).body(),
+        GraphClient.getShortestPathCar(request).body()
+    ).map {
+        val input = it.cars.first()
 
-        print(list)
-
-        val newList = list.filterNotNull()
-
-        print(newList)
-
-        return newList.map {
-            val input = it.cars.first()
-
-            val transport = when (input.vehicleType) {
-                Transport.WALK.string -> Transport.WALK
-                Transport.SUBWAY.string -> Transport.SUBWAY
-                Transport.BIKE.string -> Transport.BIKE
-                Transport.CAR.string -> Transport.CAR
-                else -> throw IllegalArgumentException()
-            }
-
-            val paths = input.paths.map { point -> Coordinate(point[0], point[1]) }
-
-            PathAdapter.Path(transport, paths, input.costs)
+        val transport = when (input.vehicleType) {
+            Transport.WALK.string -> Transport.WALK
+            Transport.SUBWAY.string -> Transport.SUBWAY
+            Transport.BIKE.string -> Transport.BIKE
+            Transport.CAR.string -> Transport.CAR
+            else -> throw IllegalArgumentException()
         }
+
+        val paths = input.paths.map { point -> Coordinate(point[0], point[1]) }
+
+        PathAdapter.Path(transport, paths, input.costs)
     }
 }
