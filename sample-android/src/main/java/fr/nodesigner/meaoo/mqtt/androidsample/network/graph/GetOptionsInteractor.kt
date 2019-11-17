554f6fc20e8dd13c6dc6f20ca8885aa11ffac76a
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2019-11-17 01:38:29 +0100

    getBikeOption

--- sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/graph/GetOptionsInteractor.kt
+++ sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/graph/GetOptionsInteractor.kt
@@ -2,19 +2,18 @@ package fr.nodesigner.meaoo.mqtt.androidsample.network.graph
 
 import fr.nodesigner.meaoo.mqtt.android.model.Coordinate
 import fr.nodesigner.meaoo.mqtt.androidsample.entity.Option
-import fr.nodesigner.meaoo.mqtt.androidsample.entity.Transport
 import kotlin.math.sqrt
 
 class GetOptionsInteractor {
 
     suspend fun execute(request: GraphService.Request): List<Option> {
         val list = listOfNotNull(
-            GraphClient.getShortestPathBike(request).body(),
             GraphClient.getShortestPathCar(request).body()
         )
 
         return listOfNotNull(
             getWalkOption(request),
+            getBikeOption(request),
             GetSubwayOptionInteractor().execute(request)
         )
     }
@@ -26,14 +25,11 @@ class GetOptionsInteractor {
         return Option.WalkToTarget(target, path.pathLength)
     }
 
-    private fun mapTransport(input: GraphService.ResultItem): Transport {
-        return when (input.vehicleType) {
-            Transport.WALK.string -> Transport.WALK
-            Transport.SUBWAY.string -> Transport.SUBWAY
-            Transport.BIKE.string -> Transport.BIKE
-            Transport.CAR.string -> Transport.CAR
-            else -> throw IllegalArgumentException()
-        }
+    private suspend fun getBikeOption(request: GraphService.Request): Option? {
+        val pathBike = GraphClient.getShortestPathBike(request).body() ?: return null
+        val path = pathBike.cars.first()
+        val target = path.paths.map { point -> Coordinate(point[0], point[1]) }.last()
+        return Option.BikeToTarget(target, path.pathLength)
     }
 }
 
