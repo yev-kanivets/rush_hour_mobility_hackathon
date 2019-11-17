c8c5b62ec424d3f3fb018a49ef70f427b2e3c355
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2019-11-17 01:12:55 +0100

    isOnSubwayStation check

--- sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/graph/GetOptionsInteractor.kt
+++ sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/graph/GetOptionsInteractor.kt
@@ -15,46 +15,8 @@ class GetOptionsInteractor {
         )
 
         return listOf(
-            subwayOption(request)
+            GetSubwayOptionInteractor().execute(request)
         )
-    }
-
-    private suspend fun subwayOption(request: GraphService.Request): Option {
-        val subwayGraph = GraphClient.getSubwayGraph().body()!!
-
-        val subwayNodes = subwayGraph.elements.nodes.map {
-            Coordinate(it.position.x / 1000, it.position.y / 1000)
-        }
-
-        return if (subwayNodes.contains(request.departure)) {
-            val subwayShortestPath = GraphClient.getShortestPathSubway(request).body()
-            if (subwayShortestPath == null) {
-                closestMetro(request.departure, subwayNodes)
-            } else {
-                val path = subwayShortestPath.cars.first()
-                val target = path.paths.map { point -> Coordinate(point[0], point[1]) }.last()
-                Option.UseMetroToTarget(target, path.pathLength)
-            }
-        } else {
-            closestMetro(request.departure, subwayNodes)
-        }
-    }
-
-    private suspend fun closestMetro(departure: Coordinate, nodes: List<Coordinate>): Option {
-        var minNode: Coordinate = nodes.first()
-        var minDist = dist(departure, minNode)
-
-        nodes.forEach {node ->
-            if (dist(departure, node) < minDist) {
-                minDist = dist(departure, node)
-                minNode = node
-            }
-        }
-
-        val walkPath = GraphClient.getShortestPathWalk(GraphService.Request(departure, minNode)).body()
-        val cost = walkPath?.cars?.first()?.pathLength ?: 0.0
-
-        return Option.WalkToSubway(minNode, cost)
     }
 
     private fun mapTransport(input: GraphService.ResultItem): Transport {
@@ -66,8 +28,8 @@ class GetOptionsInteractor {
             else -> throw IllegalArgumentException()
         }
     }
+}
 
-    private fun dist(a: Coordinate, b: Coordinate): Double {
-        return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y))
-    }
+fun dist(a: Coordinate, b: Coordinate): Double {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y))
 }
---
+++ sample-android/src/main/java/fr/nodesigner/meaoo/mqtt/androidsample/network/graph/GetSubwayOptionInteractor.kt
@@ -0,0 +1,49 @@
+package fr.nodesigner.meaoo.mqtt.androidsample.network.graph
+
+import fr.nodesigner.meaoo.mqtt.android.model.Coordinate
+import fr.nodesigner.meaoo.mqtt.androidsample.entity.Option
+
+class GetSubwayOptionInteractor {
+
+    val EPS = 0.01
+
+    suspend fun execute(request: GraphService.Request): Option {
+        val subwayGraph = GraphClient.getSubwayGraph().body()!!
+
+        val subwayNodes = subwayGraph.elements.nodes.map {
+            Coordinate(it.position.x / 1000, it.position.y / 1000)
+        }
+
+        val isOnSubwayStation = subwayNodes.count { dist(it, request.departure) < EPS } > 0
+
+        return if (isOnSubwayStation) {
+            val subwayShortestPath = GraphClient.getShortestPathSubway(request).body()
+            if (subwayShortestPath == null) {
+                closestMetro(request.departure, subwayNodes)
+            } else {
+                val path = subwayShortestPath.cars.first()
+                val target = path.paths.map { point -> Coordinate(point[0], point[1]) }.last()
+                Option.UseMetroToTarget(target, path.pathLength)
+            }
+        } else {
+            closestMetro(request.departure, subwayNodes)
+        }
+    }
+
+    private suspend fun closestMetro(departure: Coordinate, nodes: List<Coordinate>): Option {
+        var minNode: Coordinate = nodes.first()
+        var minDist = dist(departure, minNode)
+
+        nodes.forEach {node ->
+            if (dist(departure, node) < minDist) {
+                minDist = dist(departure, node)
+                minNode = node
+            }
+        }
+
+        val walkPath = GraphClient.getShortestPathWalk(GraphService.Request(departure, minNode)).body()
+        val cost = walkPath?.cars?.first()?.pathLength ?: 0.0
+
+        return Option.WalkToSubway(minNode, cost)
+    }
+}
