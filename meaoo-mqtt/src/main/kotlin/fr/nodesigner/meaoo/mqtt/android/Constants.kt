898f0d0fad3fb87418c31de3a5bdb626fd45dbc6
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2019-11-18 09:29:40 +0100

    Remove prod credentials.

--- meaoo-mqtt/src/main/kotlin/fr/nodesigner/meaoo/mqtt/android/Constants.kt
+++ meaoo-mqtt/src/main/kotlin/fr/nodesigner/meaoo/mqtt/android/Constants.kt
@@ -4,19 +4,8 @@ const val TOPIC_PREFIX = "team10"
 const val USERNAME = "team10"
 const val PASSWORD = "plnuyfnsus"
 const val NAMESPACE = "team10"
-const val SERVER_URL = "wss://mr1dns3dpz5mjj.messaging.solace.cloud:8443" //"tcp://mr1dns3dpz5mjj.messaging.solace.cloud:1883"
+const val SERVER_URL = "wss://mr1dns3dpz5mjj.messaging.solace.cloud:8443"
 
 const val GRAPH_URL = "http://graph.$NAMESPACE.xp65.renault-digital.com/"
 const val API_URL = "http://agent-controller.$NAMESPACE.xp65.renault-digital.com/"
 const val VEHICLE_URL = "http://vehicle.$NAMESPACE.xp65.renault-digital.com/"
-
-/*const val TOPIC_PREFIX = "project-65"
-const val USERNAME = "team-demo"
-const val PASSWORD = "chouchou"
-const val NAMESPACE = "p65-dev"
-const val SERVER_URL = "wss://mr1dns3dpz5mjj.messaging.solace.cloud:8443" //"tcp://mr1dns3dpz5mjj.messaging.solace.cloud:1883"
-
-const val GRAPH_URL = "http://graph.$NAMESPACE.xp65.renault-digital.com/"
-const val API_URL = "http://agent-controller.$NAMESPACE.xp65.renault-digital.com/"
-const val VEHICLE_URL = "http://vehicle.$NAMESPACE.xp65.renault-digital.com/"
-   */
